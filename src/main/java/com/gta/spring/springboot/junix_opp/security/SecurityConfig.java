25291b49b9d76b4888080d09443f8202f6ad1068
timurgareev <gareevtimura@gmail.com>
2024-05-22 23:20:43 +0300

    add security

--- src/main/java/com/gta/spring/springboot/junix_opp/security/SecurityConfig.java
+++ src/main/java/com/gta/spring/springboot/junix_opp/security/SecurityConfig.java
@@ -6,9 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -29,7 +27,7 @@ public class SecurityConfig {
     @Autowired
     private JWTAuthenticationEntryPoint jwtAuthenticationEntryPoint;
 
-    @Lazy
+
     @Autowired
     private CustomUserDetailsService customUserDetailsService;
 
